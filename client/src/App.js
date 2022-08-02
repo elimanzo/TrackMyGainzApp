84bf86e7e2a261b5a43548bcdf571ef003cea48d
unknown <96manzo.eli@gmail.com>
2022-08-01 21:43:44 -0700

    Created Form/Post(s) components to the front end Actions to action is done through redux / boilerplate code Application Connected to redux Dispatching Redux Created form to create a post and connected to the server Fetch the Post to the backend (Fully made MERN stack!) Added Post Cards on the frontend Added Background CSS Added Post functionality (Update/Delete) Created .env to store environmental variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import gainz from './images/gainz.png';
+import useStyles from './styles';
 
 const App = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect (() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch]);
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className = {classes.appBar} position="static" color="inherit">
+                <Typography variant="h2" align="center">Track My Gainz</Typography>
+                <img className={classes.image} src={gainz} alt="gainz" height="60" />
+            </AppBar>
+            <Grow in>
+                <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                    </Grid>
+                </Grid>
+            </Grow>
+        </Container>
     );
 }
 
