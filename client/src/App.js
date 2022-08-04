939245088e5dab50fc2f806fbdd831ca0f53131c
unknown <96manzo.eli@gmail.com>
2022-08-03 20:24:18 -0700

    Split components to clean up App.js Made App multipage w/ Routing Added Login/Logout Nav button for users Added Auth page for logging in/sign up (added an input.js to handle login styles) Added Google OAuth 2.0 Login/Logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import gainz from './images/gainz.png';
-import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
-const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect (() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
-    return (
-        <Container maxWidth="lg">
-            <AppBar className = {classes.appBar} position="static" color="inherit">
-                <Typography variant="h2" align="center">Track My Gainz</Typography>
-                <img className={classes.image} src={gainz} alt="gainz" height="60" />
-            </AppBar>
-            <Grow in>
-                <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId} />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                    </Grid>
-                </Grid>
-            </Grow>
-        </Container>
-    );
-}
+const App = () => (
+    <GoogleOAuthProvider clientId="233544559554-nkdhuhrvi3teeji629gl6o6ceh24sene.apps.googleusercontent.com">
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component ={Home} />
+                    <Route path="/auth" exact component ={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
+    </GoogleOAuthProvider>
+);
 
 export default App;
